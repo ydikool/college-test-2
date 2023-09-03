59bab8b4ea943b83b04d8a470a587d481b23c2e4
ydikool <irina.xrapkova@gmail.com>
2023-09-03 09:55:41 +0300

    first step fixed

--- src/solution.js
+++ src/solution.js
@@ -1,4 +1,20 @@
 export default (content) => {
-  const rows = content.split('\n').slice(1);
-  console.log(`Колличество рядов существ: ${rows.length}`);
+  const data = content.split('\n').slice(1);
+  console.log(`Количество рядов: ${data.length}`);
+
+  const rows = data.map((row) => row
+  .split('|')
+  .filter((row)=>row))
+  .map((array) => array
+    .map((element) => element.trim())
+  );
+
+  const strength = rows.map((row) => Number(row[1]));
+  const strongestIndex = strength.indexOf(Math.max(...strength));
+  console.log(strongestIndex);
+  const strenghtWithoutStrongest = strength.slice()
+  strenghtWithoutStrongest[strongestIndex]=0
+  const secondStrongestIndex = strength.indexOf(Math.max(...strenghtWithoutStrongest));
+  console.log(strenghtWithoutStrongest);
+  console.log(secondStrongestIndex);
 };
